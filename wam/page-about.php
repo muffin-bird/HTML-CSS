9241a259c9874771df8b67a1eccc420bcd465d67
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-07-20 06:55:16 +0900

    add custom

--- wam/page-about.php
+++ wam/page-about.php
@@ -1,14 +1,19 @@
 <?php get_header(); ?>
 
+<?php
+/*
+Template Name: About
+*/
+?>
+
 <section class="section">
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
   <div class="page-headline"><?php the_title(); ?></div>
   <div class="page-about">
-    <img class="about-img" src="<?php echo get_template_directory_uri(); ?>/img/about.jpg" width="250" height="250" alt="about">
+    <img class="about-img" src="<?php echo CFS()->get('about_img'); ?>">
     <div class="about-info">
       <div class="about-description">
-        <?php the_content(); ?>
-      </div>
+        <?php echo CFS()->get('about_description'); ?>
+      </div> 
       <div class="sns-icon">
         <div class="twitter-icon">
           <a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/twitter.svg" width="40" height="40"></a>
@@ -25,7 +30,6 @@
       </div>
     </div>
   </div>
-  <?php endwhile; endif; ?>
 </section>
 
 <?php get_footer(); ?>

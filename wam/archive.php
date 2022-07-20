c91d8187232f0207fca088fbdb50902a400d4ed1
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-07-21 08:20:34 +0900

    change archive

--- wam/archive.php
+++ wam/archive.php
@@ -1,14 +1,18 @@
 <?php get_header(); ?>
 
 <div class="section">
-    <ul class="blog-list">
+  <div class="page-headline archive-headline">Blog</div>
+    <ul class="archive-blog-list">
       <?php if (have_posts()) : while (have_posts()) : the_post(); ?><!-- ループ開始 -->
-      <li class="blog-item">
-        <a href="<?php the_permalink(); ?>" class="blog-link">
-          <time class="blog-data"><?php echo get_the_date('Y.m.d'); ?></time>
-          <div class="blog-info">
-            <h2 class="blog-title"><?php the_title(); ?></h2>
-            <p class="blog-ttl"><?php the_content(); ?></p>
+      <li class="archive-blog-item">
+        <a href="<?php the_permalink(); ?>" class="archive-blog-link">
+          <div class="archive-blog-img">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/blog.jpg" width="180" height="180" alt="blog">
+          </div>
+          <div class="archive-blog-info">
+            <time class="archive-blog-data"><?php echo get_the_date('Y.m.d'); ?></time>
+            <h2 class="archive-blog-title"><?php the_title(); ?></h2>
+            <p class="archive-blog-ttl"><?php the_content(); ?></p>
           </div>
         </a>
       </li>

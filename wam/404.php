5d3f45bbcc9697551847e8e675652005fb086bf0
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-08-15 20:57:11 +0900

    Create 404.php

---
+++ wam/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<section class="section">
+  <h2 class="page-headline">404 NOT FOUND</h2>
+  <div class="container">
+    <p>お探しのページが見つかりませんでした。</p>
+    <p>申し訳ございませんが、<a href="<?php echo esc_url(home_url('/')); ?>">こちらのリンク</a>からトップページにお戻りください。</p>
+  </div>
+</section>
+
+<?php get_footer(); ?>

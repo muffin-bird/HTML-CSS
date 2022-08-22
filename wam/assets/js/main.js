a6ff985ad3bd67fdbb3b4391b4da10138bb5b0e1
Yuuu <86790336+muffin-bird@users.noreply.github.com>
2022-08-22 11:11:30 +0900

    Add fadeUp animation

--- wam/assets/js/main.js
+++ wam/assets/js/main.js
@@ -1,26 +1,43 @@
 jQuery(function($){
 
-	function fadeAnime(){
+	function delayScrollAnime(){
+		var time = 0.3; // 遅延時間の値
+		var value = time;
+		$('.archive-works-item').each(function(){
+			var parent = this; // 親要素を取得
+			var elemPos = $(this).offset().top; // 要素の位置を取得
+			var scroll = $(window).scrollTop(); // スクロール量を取得
+			var windowHeight = $(window).height(); // 画面の高さを取得
+			var childs = $(this).children(); // 子要素を取得
 
-		// 動きの指定
-		$('.archive-works-item').each(function(){ // クラス名の指定
-			var elemPos = $(this).offset().top-50; // 要素より、50px上の
-			var scroll = $(window).scrollTop();
-			var windowHeight = $(window).height();
-			if (scroll >= elemPos - windowHeight){
-			$(this).addClass('fadeIn'); // 画面内に入ったらfadeInというクラス名を追記
-			}else{
-			$(this).removeClass('fadeIn'); // 画面外に出たらfadeInというクラス名を外す
+			if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")){ // 指定領域内,親要素にクラス(!play)
+				$(childs).each(function() {
+					if(!$(this).hasClass("fadeUp")) { // アニメーションのクラスが指定されているかチェック
+						$(parent).addClass("play"); // 親要素にクラス(play)を追加
+						$(this).css("animation-delay", value + "s"); // CSS追加
+						$(this).addClass("fadeUp"); // アニメーションクラスの追加
+						value = value + time; // delay時間の増加
+
+						var index = $(childs).index(this); // 要素のindex番号の取得
+						if((childs.length-1) == index) {
+							$(parent).removeClass("play"); // クラス(play)を外す
+						}
+					}
+				})
+			} else {
+				$(childs).removeClass("fadeUp"); // アニメーションクラスの削除
+				value = time; // delayの初期値
 			}
-		});
-	
+		})
 	}
 	
 		// 画面スクロール
 		$(window).scroll(function (){
-	
-			fadeAnime(); // 関数の呼び出し
-	
+			delayScrollAnime();
 		});
 
+		// 画面が読み込み
+		$(window).on('load', function() {
+			delayScrollAnime();
+		});
 });

e3d7b7498700d6216817d0db54e9a91e005e68f4
Yuuu <86790336+muffin-bird@users.noreply.github.com>
2022-08-24 08:15:50 +0900

    Change Font

--- wam/header.php
+++ wam/header.php
@@ -8,7 +8,7 @@
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
   <?php
-  wp_enqueue_style('google-font', 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap');
+  wp_enqueue_style('google-font', 'https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap');
   wp_enqueue_script('jquery');
   wp_enqueue_script('wam-main', get_template_directory_uri() . '/assets/js/main.js');
   wp_head();

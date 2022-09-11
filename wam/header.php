480408a8f1e70e6183873f8043c92983d13e743a
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-09-11 17:49:31 +0900

    Add ViewPort

--- wam/header.php
+++ wam/header.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="<?php bloginfo('description'); ?>">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">

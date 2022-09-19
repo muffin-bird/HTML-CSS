049688a2877df2ba83279ed4817bd66430091fb7
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-09-20 01:24:22 +0900

    Add Front Responsive 760

--- wam/assets/js/main.js
+++ wam/assets/js/main.js
@@ -98,12 +98,15 @@ jQuery(function ($) {
 		});
 	});
 
-	$(".burger").click(function () { // ナビゲーション
+	/*
+	ナビゲーション
+	*/
+	$(".burger").click(function () {
 		$(this).toggleClass('active'); // activeクラスを付与
 		$(".nav-list").toggleClass('panelactive'); // panelactiveクラス付与
 	});
-	
-	$(".nav-list a").click(function () {　
+
+	$(".nav-list a").click(function () {
 		$(".burger").removeClass('active'); // activeクラス除去
 		$(".nav-list").removeClass('panelactive'); // panelactiveクラス除去
 	});

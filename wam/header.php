72cd91ac8732a99564aebab2ded4d1d218aec8a4
muffin-bird <86790336+muffin-bird@users.noreply.github.com>
2022-11-11 03:02:29 +0900

    Update header.php

--- wam/header.php
+++ wam/header.php
@@ -2,15 +2,22 @@
 <html lang="ja">
 
 <head>
-  <meta charset="UTF-8">
+  <!-- Google Tag Manager -->
+  <!-- End Google Tag Manager -->
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="<?php bloginfo('description'); ?>">
+  <meta name="format-detection" content="telephone=no">
+  <?php if(is_search()): ?>
+    <meta name="robots" content="noindex">
+  <?php elseif(is_404()): ?>
+    <meta name="robots" content="noindex">
+  <?php endif; ?>
+  <?php get_template_part('template-parts/seo-header'); ?>
+  <?php get_template_part('template-parts/ogp'); ?> 
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/destyle.css">
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
-
   <?php
-  wp_enqueue_style('google-font', 'https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap');
   wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css');
   wp_enqueue_style('slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css');
   wp_enqueue_style('slick-theme', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css');
@@ -23,6 +30,8 @@
 </head>
 
 <body>
+  <!-- Google Tag Manager (noscript) -->
+  <!-- End Google Tag Manager (noscript) -->
   <div id="loader-bg">
     <div id="loader">
       <span></span>
@@ -34,7 +43,7 @@
   <div id="contents">
     <header class="header">
       <div class="nav-logo">
-        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.svg" width="50" height="50" alt="ロゴ"></a>
+        <a href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/staff.svg" width="146" height="26" alt="logo"></a>
         <div class="burger"><span></span><span></span><span></span></div>
         <nav class="nav-list">
           <ul>
